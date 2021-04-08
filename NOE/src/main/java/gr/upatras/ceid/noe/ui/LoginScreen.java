d6f1beac3146952380ea9f8586fe6aafc2962930
Evangelos Lioumis <>
2021-04-08 12:32:02 +0300

    NOE-37 Login screen centered / Small code clean-up

--- NOE/src/main/java/gr/upatras/ceid/noe/ui/LoginScreen.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/ui/LoginScreen.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gr.upatras.ceid.noe.ui;
 
 /**
  *
- * @author User
+ * @author Evangelos Lioumis
  */
 public class LoginScreen extends javax.swing.JFrame {
 
-    /**
-     * Creates new form LoginScreen
-     */
+
     public LoginScreen() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -123,9 +118,7 @@ public class LoginScreen extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jPasswordField1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -150,7 +143,7 @@ public class LoginScreen extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new LoginScreen().setVisible(true);
