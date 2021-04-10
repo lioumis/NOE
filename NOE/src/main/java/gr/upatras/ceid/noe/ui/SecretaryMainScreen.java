23c111c90d2a5f6fa83521964d99dad531416ee1
Evangelos Lioumis <>
2021-04-10 21:38:19 +0300

    NOE-37 Small Clean-up

--- NOE/src/main/java/gr/upatras/ceid/noe/ui/SecretaryMainScreen.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/ui/SecretaryMainScreen.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gr.upatras.ceid.noe.ui;
 
 /**
  *
- * @author User
+ * @author Evangelos Lioumis
  */
 public class SecretaryMainScreen extends javax.swing.JFrame {
 
-    /**
-     * Creates new form SecretaryMainScreen
-     */
     public SecretaryMainScreen() {
         initComponents();
         this.setLocationRelativeTo(null);
