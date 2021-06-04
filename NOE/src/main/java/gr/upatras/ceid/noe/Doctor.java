5dee92c24cdf2cca43f1e1cfc252f1a39bd37a8f
NickSchizas <sxiznik@gmail.com>
2021-06-05 01:26:30 +0300

    Προσθήκη Κλάσεων.

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Doctor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author NickSxiz
+ */
+public class Doctor {
+    private String speciality;
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+    
+     public Doctor getDoctor(Doctor Doctor) {
+        return Doctor;
+    }
+}
---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Sensor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author NickSxiz
+ */
+public class Sensor {
+    
+
+    public void activate(){
+}
+    
+    public String getData(){
+        return new String();
+    }
+
+    public void deactivate() {
+    }
+    
+    public void restart(){
+    }
+}
