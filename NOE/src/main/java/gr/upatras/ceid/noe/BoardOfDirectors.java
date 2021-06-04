7bf7e7594064f6ea9cd29d05ad62b14e439205d1
Manos <manosboursalis@gmail.com>
2021-06-05 01:38:43 +0300

    Creating BoardOfDirectors class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/BoardOfDirectors.java
@@ -0,0 +1,24 @@
+
+package gr.upatras.ceid.noe;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Emmanouil Boursalis
+ */
+public class BoardOfDirectors {
+    private Set<String> members;
+
+    public Set<String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<String> members) {
+        this.members = members;
+    }
+    
+    
+    
+    
+}
