6828b41e48c9deb37cc33993b471aca99d53c317
NickSxiz <n.sxi13@gmail.com>
2021-06-05 17:08:55 +0300

    Add methods to DoctorProfile

--- NOE/src/main/java/gr/upatras/ceid/noe/ui/DoctorProfile.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/ui/DoctorProfile.java
@@ -23,7 +23,7 @@ public class DoctorProfile extends javax.swing.JFrame {
     }
     
     private void displayEvaluations(){
-        
+         
     }
     /**
      * This method is called from within the constructor to initialize the form.
