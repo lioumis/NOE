1fd3b0f979fdcb9a60f2f9b5155de7536a42a4d9
DimitraLirou <dimitralirou@gmail.com>
2021-06-05 16:23:33 +0300

    NOE-90 create CryptographyHelper Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/CryptographyHelper.java
@@ -0,0 +1,16 @@
+package gr.upatras.ceid.noe;
+
+public class CryptographyHelper {
+
+    private String key;
+
+    public String encrypt(){
+        return null;
+    }
+
+    public String decrypt(){
+        return null;
+    }
+
+
+}
