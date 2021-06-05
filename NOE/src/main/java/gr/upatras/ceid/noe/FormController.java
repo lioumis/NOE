9de925f279e239552ce07cd2c97ebd297aba3e25
NickSxiz <n.sxi13@gmail.com>
2021-06-05 14:36:19 +0300

    Add FormController Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/FormController.java
@@ -0,0 +1,22 @@
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author NickSxiz
+ */
+public class FormController {
+
+    public boolean checkForErrors(){
+        return true;
+
+    }
+
+    public boolean checkForEmpties(){
+        return true;
+
+    }
+
+    public void save(){
+
+    }
+}
