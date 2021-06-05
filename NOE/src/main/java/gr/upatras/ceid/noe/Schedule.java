d8366c2edb924209d26e38e7322bdbf238fc91f2
NickSxiz <n.sxi13@gmail.com>
2021-06-05 14:13:37 +0300

    Add Schedule Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Schedule.java
@@ -0,0 +1,43 @@
+package gr.upatras.ceid.noe;
+import java.util.*;
+import java.time.LocalDateTime;
+/**
+ *
+ * @author NickSxiz
+ */
+public class Schedule {
+    private HashMap<LocalDateTime, Appointment> appointments;
+
+    public HashMap<LocalDateTime, Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(HashMap<LocalDateTime, Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
+    private HashMap<LocalDateTime, Surgery> surgeries;
+
+    private HashMap<LocalDateTime, LocalDateTime> unavailable;
+
+
+    public HashMap<LocalDateTime, LocalDateTime> getUnavailable() {
+        return unavailable;
+    }
+
+    public void setUnavailable(HashMap<LocalDateTime, LocalDateTime> unavailable) {
+        this.unavailable = unavailable;
+    }
+
+    public HashMap<LocalDateTime, Surgery> getSurgeries() {
+        return surgeries;
+    }
+
+    public void setSurgeries(HashMap<LocalDateTime, Surgery> surgeries) {
+        this.surgeries = surgeries;
+    }
+
+    public Schedule getSchedule(){
+        return new Schedule();
+    }
+}
