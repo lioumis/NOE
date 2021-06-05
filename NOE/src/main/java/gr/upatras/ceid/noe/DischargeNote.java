32f48fbe24b784709bf0a39f68c50794763afb29
Manos <manosboursalis@gmail.com>
2021-06-05 12:24:14 +0300

    Creating DischargeNote Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/DischargeNote.java
@@ -0,0 +1,23 @@
+
+package gr.upatras.ceid.noe;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Emmanouil Boursalis
+ */
+public class DischargeNote {
+    private ArrayList<String> diseases;
+
+    public ArrayList<String> getDiseases() {
+        return diseases;
+    }
+
+    public void setDiseases(ArrayList<String> diseases) {
+        this.diseases = diseases;
+    }
+    
+
+   
+}
