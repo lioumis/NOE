376c6aa260c914ef0cd2cdc4091be50ae62c064c
Manos <manosboursalis@gmail.com>
2021-06-05 15:30:16 +0300

    Changes in TreatmentCost Class

--- NOE/src/main/java/gr/upatras/ceid/noe/TreatmentCost.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/TreatmentCost.java
@@ -2,21 +2,20 @@
 package gr.upatras.ceid.noe;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
  * @author Emmanouil Boursalis
  */
 public class TreatmentCost {
-    private HashMap<String, float> costs;
+    private HashMap<String, Float> costs;
 
-    public HashMap<String, float> getCosts() {
+    public HashMap<String, Float> getCosts() {
         return costs;
     }
 
     
-    public void setCosts(HashMap<String, float> costs) {
+    public void setCosts(HashMap<String, Float> costs) {
         this.costs = costs;
     }
     
