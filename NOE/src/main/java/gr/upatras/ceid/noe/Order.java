10961ac1aa005582abba8f8cfb7b8cdfcb5233a1
Manos <manosboursalis@gmail.com>
2021-06-05 12:35:00 +0300

    Creating Order Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Order.java
@@ -0,0 +1,36 @@
+
+package gr.upatras.ceid.noe;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Emmanouil Boursalis
+ */
+public class Order {
+    private HashMap<Supply, int> supplies;
+    private LocalDateTime date;
+
+    public HashMap<Supply, <any>> getSupplies() {
+        return supplies;
+    }
+
+    public void setSupplies(HashMap<Supply, <any>> supplies) {
+        this.supplies = supplies;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    public void save(){
+     return;   
+    }
+    
+    
+    
+}
