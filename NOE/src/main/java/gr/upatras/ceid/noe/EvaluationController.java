a1010eff54fc49d9a59ce2cb2c305519a2145719
NickSchizas <sxiznik@gmail.com>
2021-06-05 12:20:17 +0300

    Προσθήκη κλάσης EvaluationController

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/EvaluationController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author User
+ */
+public class EvaluationController {
+    
+    public void calculateEvaluations(){
+        
+    }
+    
+    public void display(){
+        
+    }
+    
+    public void displayForm(){
+        
+    }
+    
+}
