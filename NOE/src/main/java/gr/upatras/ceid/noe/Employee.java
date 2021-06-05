9197ed1bee020e941da3e41a64783b5d11c3fdfb
DimitraLirou <dimitralirou@gmail.com>
2021-06-05 16:38:44 +0300

    NOE-90 create Employee Class

--- NOE/src/main/java/gr/upatras/ceid/noe/Employee.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/Employee.java
@@ -1,2 +1,33 @@
-package gr.upatras.ceid.noe;public class Employee {
+package gr.upatras.ceid.noe;
+
+public class Employee {
+
+    private int salary;
+    private Hospital hospital;
+    private String department;
+
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
