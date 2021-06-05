6a7f7688e25de93f17e668c7cbc8c5fd5fdb2ee7
DimitraLirou <dimitralirou@gmail.com>
2021-06-05 16:17:29 +0300

    NOE-90 create MessageHelper Class

--- NOE/src/main/java/gr/upatras/ceid/noe/MessageHelper.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/MessageHelper.java
@@ -1,2 +1,10 @@
-package gr.upatras.ceid.noe;public class MessageHelper {
+package gr.upatras.ceid.noe;
+
+public class MessageHelper{
+
+    public String showSuccessMessage;
+    public String showErrorMessage;
+    public boolean showEvaluationProposalMessage;
+    public boolean showConfirmationMessage;
+    private void close(){}
 }
