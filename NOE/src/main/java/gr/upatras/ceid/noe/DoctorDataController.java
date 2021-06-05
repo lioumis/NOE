d503b9aa74d331073e0c5836569dd1921187b8dd
Dimitra Lyrou <Dimitra Lyrou@DESKTOP-KH5AHV2>
2021-06-05 11:37:49 +0300

    Δημιουργία της κλάσης DoctorDataController

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/DoctorDataController.java
@@ -0,0 +1,18 @@
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author Dimitra Lyrou
+ */
+public class DoctorDataController {
+    
+    public void getData(){}
+    private boolean completitionCheck;
+    public Appointment cancelAppointment;
+    public boolean isCorrect;
+    public boolean okData;
+    public void save(){}
+    
+    
+    
+}
