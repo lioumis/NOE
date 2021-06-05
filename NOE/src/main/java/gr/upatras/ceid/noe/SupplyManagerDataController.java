d0282658b68d252c0e142508e6936264cb62ceca
Manos <manosboursalis@gmail.com>
2021-06-05 12:41:25 +0300

    Creating SupplyManagerDataController Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/SupplyManagerDataController.java
@@ -0,0 +1,20 @@
+
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author Emmanouil Boursalis
+ */
+public class SupplyManagerDataController {
+    public void askData(){
+        return;
+    }
+    
+    private void calculate(){
+        return;
+    }
+    
+    public void create(){
+        return;
+    }
+}
