94297d74980877af9c618536fb8a578cf8f31de8
NickSchizas <sxiznik@gmail.com>
2021-06-05 12:17:24 +0300

    Προσθήκη κλάσης Surgery

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Surgery.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.upatras.ceid.noe;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+/**
+ *
+ * @author NickSxiz
+ */
+public class Surgery {
+    private LocalDateTime dateTime;
+    private int duration;
+    private ArrayList<Doctor> doctors;
+    private Patient patient;
+    
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public ArrayList<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(ArrayList<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+   
+    public String getSurgery(){
+        return new String();
+    }
+    
+}
