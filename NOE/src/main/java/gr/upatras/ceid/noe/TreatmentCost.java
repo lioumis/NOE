9ec3d2878eab00d84d7c155caf3cb13505ba08cf
Manos <manosboursalis@gmail.com>
2021-06-05 11:51:25 +0300

    Creating TreatmentCost class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/TreatmentCost.java
@@ -0,0 +1,28 @@
+
+package gr.upatras.ceid.noe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Emmanouil Boursalis
+ */
+public class TreatmentCost {
+    private HashMap<String, float> costs;
+
+    public HashMap<String, float> getCosts() {
+        return costs;
+    }
+
+    
+    public void setCosts(HashMap<String, float> costs) {
+        this.costs = costs;
+    }
+    
+   public void updateCost(){
+   
+   }
+
+    
+}
