4da51b31f65314afec6b6b027645528ab1741dfe
NickSchizas <sxiznik@gmail.com>
2021-06-05 12:16:49 +0300

    Προσθήκη κλάσης Evaluation

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Evaluation.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gr.upatras.ceid.noe;
+
+/**
+ *
+ * @author NickSxiz
+ */
+public class Evaluation {
+    private Doctor doctor;
+    
+    private Patient patient;
+    
+    private int rating;
+    
+    private String comment;
+    
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+    public Evaluation getEvaluation(){
+        return new Evaluation();
+    }
+    
+    
+}
