4fbe039ec0f4ce286a530e63030649b6ff83af5e
DimitraLirou <dimitralirou@gmail.com>
2021-06-05 23:04:49 +0300

    NOE-90 changes

--- NOE/src/main/java/gr/upatras/ceid/noe/DatabaseConnection.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/DatabaseConnection.java
@@ -1,2 +1,95 @@
-package gr.upatras.ceid.noe;public class DatabaseConnection {
+package gr.upatras.ceid.noe;
+import java.util.*;
+import java.util.ArrayList;
+
+public class DatabaseConnection {
+
+    String Patient;
+    String Surgery;
+    String Schedule;
+    private java.util.ArrayList<Supply> Supplies;
+    private Object String;
+
+
+    public int retrieveAvailableBeds(){
+        return 0;
+    }
+
+    public HashMap<String, Integer> RetrieveDiseases(){
+        return new HashMap <String, Integer> RetrieveDiseases();
+    }
+
+    public int retrieveDeaths(){
+        return 0;
+    }
+
+    public int retrieveAdmissions(){
+        return 0;
+    }
+
+    public int retrieveHospitalizations(){
+        return 0;
+    }
+
+    public ArrayList<EmployeeEmails> retrieveEmployeeEmails(){
+        return new ArrayList<EmployeeEmails>;
+    }
+
+    public String retrieveMinistryEmail(){
+        return null;
+    }
+
+    public void retrieveBudgetInfo(){}
+
+    public void updateBudgetInfo(HospitalBudget){}
+
+    public String searchPatient(){
+        return Patient;
+    }
+
+    public String searchHospitalizationCost(){
+        return TreatmentCost;
+    }
+
+    public TreatmentCost updateCost(){
+        return null;
+    }
+
+    public ArrayList<Doctor> retrieveDoctors(){
+        return ArrayList<Doctor>;
+    }
+
+    public ArrayList<Evaluation> retrieveEvaluations(){
+        return ArrayList<Evaluation>;
+    }
+
+    public ArrayList<Appointment> retrieveAppointments(){
+        return ArrayList<Appointment>;
+    }
+
+    public ArrayList<Patient> retrievePatients(){
+        return ArrayList<Patient>;
+    }
+
+    public ArrayList<String> retrieveEmails(){
+        return ArrayList<String>;
+    }
+
+    public String getSurgery(){
+        return Surgery;
+    }
+
+    public String getSchedule(){
+        return Schedule;
+    }
+
+    public ArrayList<Supply> getSupplies(){
+        return Supplies;
+    }
+
+    public String retrieveCryptoKey(){
+        return null;
+    }
+
+
 }
