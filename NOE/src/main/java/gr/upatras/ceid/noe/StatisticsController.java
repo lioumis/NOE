bfbe9719e3cdaf220c3c3077188d146b5931dd1a
lioumis <up1054325@upnet.gr>
2021-06-05 16:35:46 +0300

    NOE-87 Statistics added / Corrections

--- NOE/src/main/java/gr/upatras/ceid/noe/StatisticsController.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/StatisticsController.java
@@ -1,81 +1,16 @@
 package gr.upatras.ceid.noe;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 /**
  * @author Evangelos Lioumis
  */
 public class StatisticsController {
-    private LocalDateTime from;
-    private LocalDateTime to;
-    private HashMap<String, Integer> diseases;
-    private int deaths;
-    private int availableBeds;
-    private int hospitalizations;
-    private ArrayList<TreatmentCost> treatmentCosts;
-
-    public LocalDateTime getFrom() {
-        return from;
-    }
-
-    public void setFrom(LocalDateTime from) {
-        this.from = from;
-    }
-
-    public LocalDateTime getTo() {
-        return to;
-    }
-
-    public void setTo(LocalDateTime to) {
-        this.to = to;
-    }
-
-    public HashMap<String, Integer> getDiseases() {
-        return diseases;
-    }
-
-    public void setDiseases(HashMap<String, Integer> diseases) {
-        this.diseases = diseases;
-    }
-
-    public int getDeaths() {
-        return deaths;
-    }
-
-    public void setDeaths(int deaths) {
-        this.deaths = deaths;
-    }
-
-    public int getAvailableBeds() {
-        return availableBeds;
-    }
-
-    public void setAvailableBeds(int availableBeds) {
-        this.availableBeds = availableBeds;
-    }
-
-    public int getHospitalizations() {
-        return hospitalizations;
-    }
-
-    public void setHospitalizations(int hospitalizations) {
-        this.hospitalizations = hospitalizations;
-    }
-
-    public ArrayList<TreatmentCost> getTreatmentCosts() {
-        return treatmentCosts;
-    }
-
-    public void setTreatmentCosts(ArrayList<TreatmentCost> treatmentCosts) {
-        this.treatmentCosts = treatmentCosts;
-    }
-
-    public void initializeStatistics() {
+    public void calculateStatistics() {
 
     }
 
-    private void calculateStatistics() {
+    public void createEmail(ArrayList<String> recepients, String subject, String body) {
 
     }
 }
