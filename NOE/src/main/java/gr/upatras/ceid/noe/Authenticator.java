bb939f56afad18b5f062480c47031ce7c164f137
DimitraLirou <dimitralirou@gmail.com>
2021-06-05 17:10:07 +0300

    NOE-90 create Authenticator Class

---
+++ NOE/src/main/java/gr/upatras/ceid/noe/Authenticator.java
@@ -0,0 +1,53 @@
+package gr.upatras.ceid.noe;
+
+import java.util.ArrayList;
+
+public class Authenticator {
+
+    private String username;
+    private String password;
+    private String oneTimePassword;
+    private ArrayList<String> roles;
+    private int failedAttempts;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOneTimePassword() {
+        return oneTimePassword;
+    }
+
+    public void setOneTimePassword(String oneTimePassword) {
+        this.oneTimePassword = oneTimePassword;
+    }
+
+    public ArrayList<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<String> roles) {
+        this.roles = roles;
+    }
+
+    public int getFailedAttempts() {
+        return failedAttempts;
+    }
+
+    public void setFailedAttempts(int failedAttempts) {
+        this.failedAttempts = failedAttempts;
+    }
+
+}
