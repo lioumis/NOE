4724c50f5b6f578bb50771257d6e761ad71fc76d
lioumis <up1054325@upnet.gr>
2021-06-09 19:15:33 +0300

    Useless screen removal

--- NOE/src/main/java/gr/upatras/ceid/noe/ui/PatientMainScreen.java
+++ NOE/src/main/java/gr/upatras/ceid/noe/ui/PatientMainScreen.java
@@ -407,7 +407,7 @@ public class PatientMainScreen extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        PatientAppointmentsScreen.main(null);
+
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
